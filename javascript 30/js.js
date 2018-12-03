dd4e0cb9f80d3798f7e002df0464bc65795f43f5
hiren <hiren@freshbits.in>
2018-12-03 17:49:31 +0530

    js30 continue

--- javascript 30/js.js
+++ javascript 30/js.js
@@ -1,12 +1,25 @@
-const panels = document.querySelectorAll('.panel');
-function toggleOpen () {
-    this.classList.toggle('open');
-}
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
 
-function toggleActive(e) {
-    if(e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-    }
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+    if(!isDrawing) return;
+    console.log(e);
+    ctx.beginPath();
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 }
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mousedown',() => isDrawing = true);
+canvas.addEventListener('mouseup',() => isDrawing = false);
+canvas.addEventListener('mouseout',() => isDrawing = false);
