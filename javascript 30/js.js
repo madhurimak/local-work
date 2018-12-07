77f48b8ac2536e504245a6063be06f5b0712d97d
hiren <hiren@freshbits.in>
2018-12-07 18:36:01 +0530

    js30

--- javascript 30/js.js
+++ javascript 30/js.js
@@ -1,27 +1,13 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function addItem(e) {
-    e.preventDefault();
-    const text = (this.querySelector('[name=item]')).value;
-    const item = {
-        text,
-        done: false
-    };
-    item.push(item);
-    populateList(items, itemsList);
-    this.reset();
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an)/i, '').trim();
 }
 
-function populateList(plates = [], platesList) {
-    platesList.innerHTML = plates.map((plate, i) => {
-        return `
-        <li>
-            <label>${plate.text}</label>
-        </li>
-        `;
-    }).join('');
-}
+const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
 
-addItems.addEventListener('submit', addItems);
+document.querySelector('#bands').innerHTML =
+sortedBands
+.map(band => `<li>${band}</li>`)
+.join('')
+console.log(sortedBands);
