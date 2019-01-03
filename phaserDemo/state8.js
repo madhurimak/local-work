323c12bf8b9e4dace14dc2b941d07f08d8ed38cc
hiren <hiren@freshbits.in>
2019-01-03 18:34:48 +0530

    animated canvas continue

--- phaserDemo/state8.js
+++
@@ -1,9 +0,0 @@
-demo.state0 = function(){};
-demo.state0.prototype = {
-    preload: function(){},
-    create: function(){
-        game.stage.backgrounColor = '#80ff80';
-        console.log('state0')
-    },
-    update: function(){}
-};
